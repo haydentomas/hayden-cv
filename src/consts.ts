55f66cd070306fea229955d61df32914286b43d4
pixaful <goteamtomas@gmail.com>
2024-06-12 21:03:02 +0100

    Consts update for site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Hayden Tomas - Web Developer';
+export const SITE_DESCRIPTION = 'I write code and build websites. I also like to write about code and web development.';
